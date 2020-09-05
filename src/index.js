2b21e18ec36584d7aedf41f91fed380cf3e11c24
felis392 <maitake7shimeji@gmail.com>
2020-09-06 01:02:51 +0900

    package.json exports add

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export {dropWhile} from './dropWhile.js';
+export {filter} from './filter.js';
+export {forEach} from './forEach.js';
+export {Iteration} from './Iteration.js';
+export {map} from './map.js';
+export {peek} from './peek.js';
+export {takeWhile} from './takeWhile.js';
